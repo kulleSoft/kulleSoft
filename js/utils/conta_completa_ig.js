44269f12e056a85eb820a5c1e81667c4e83dca84
kulleSoft <59654983+kulleSoft@users.noreply.github.com>
2021-12-29 22:45:38 -0300

    Update conta_completa_ig.js

--- js/utils/conta_completa_ig.js
+++ js/utils/conta_completa_ig.js
@@ -1,4 +1,27 @@
+ let saldo = ""
 
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        let uid = user.uid;
+        firebase.database().ref('usuario/' + uid).once('value', (sanpshot) => {
+            saldo = parseFloat(sanpshot.val().valor);
+            document.querySelector('.valor').innerText = "R$ " + (saldo).toLocaleString('pt-BR', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            })
+        })
+        window.setInterval(atualizar_saldo, 3000);
+        const atualizar_saldo = () => {
+            saldo = parseFloat(sanpshot.val().valor);
+            document.querySelector('.valor').innerText = "R$ " + (saldo).toLocaleString('pt-BR', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            })
+        }
+    } else {
+        window.location.replace("https://localhost:8081");
+    }
+});
 let numero_estoque ="4";
 
 window.setInterval(numero_contas, 3000);
