04ec89a2779327870e56051ed4f2a86bbd937959
kulleSoft <59654983+kulleSoft@users.noreply.github.com>
2022-08-06 15:18:10 -0300

    kullesoft

--- js/database/firebase.js
+++ js/database/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
- apiKey: "AIzaSyCY_xWBSc3l91fp32tHvmFOL3MnA0wfcsg",
-  authDomain: "kestplus-318e4.firebaseapp.com",
-  databaseURL: "https://kestplus-318e4-default-rtdb.firebaseio.com",
-  projectId: "kestplus-318e4",
-  storageBucket: "kestplus-318e4.appspot.com",
-  messagingSenderId: "799455639786",
-  appId: "1:799455639786:web:1beb591d13a26008623f93"
-  };
+  apiKey: "AIzaSyDaLRm7hFox11uU9374CVaSFanHsUwjIqQ",
+  authDomain: "gnibot-90050.firebaseapp.com",
+  databaseURL: "https://gnibot-90050-default-rtdb.firebaseio.com",
+  projectId: "gnibot-90050",
+  storageBucket: "gnibot-90050.appspot.com",
+  messagingSenderId: "330244485531",
+  appId: "1:330244485531:web:86a743eb922f88ebcaf13e"
+};
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
