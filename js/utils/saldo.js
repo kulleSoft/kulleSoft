67d87eacbe9e20a8a04690523e6db2e9614d1246
kulleSoft <kullesoft@gmail.com>
2022-08-09 16:22:33 -0300

    kullesoft

--- js/utils/saldo.js
+++ js/utils/saldo.js
@@ -3,13 +3,16 @@ let saldo = ""
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         let uid = user.uid;
-        firebase.database().ref('usuario/' + uid).once('value', (sanpshot) => {
+        firebase.database().ref('DIZU_SERVER/Contas/' + uid).once('value', (sanpshot) => {
             saldo = parseFloat(sanpshot.val().valor);
             document.querySelector('.valor').innerText = "R$ " + (saldo).toLocaleString('pt-BR', {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2
             })
+			document.querySelector('.nome_user').innerText = sanpshot.val().nome;
         })
+		 
+        
         window.setInterval(atualizar_saldo, 3000);
         const atualizar_saldo = () => {
             saldo = parseFloat(sanpshot.val().valor);
